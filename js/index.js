4e99d7fc83ae7e0ef66f9344727e0b524284557e
vasyl <vasyl.fear.k2@gmail.com>
2019-05-13 14:09:22 +0000

    done

--- js/index.js
+++ js/index.js
@@ -153,10 +153,67 @@ let buttonArr;
 let gameOver = false;
 let gameDraw = false;
 let gameMode = 0;
-
+let numberMove;
+let vinPosZero = [[2,3,4],[4,5,6],[1,8,5],[2,8,6],[3,8,7]];
 
 function getGameMode() {
     Math.random() * 2 > 1 ? gameMode = 1 : gameMode = 0;
+}
+
+function getArrayTemp(stepArray, vinPos) {
+
+    function getArray(stepArray) {
+        let check = 0;
+        let arrVinTemp = [...stepArray];
+        let index = 0;
+        let move = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        stepArray.forEach(val => move = move.filter(el => el !== val));
+        while (move.length > 0) {
+            check = 0;
+            while (check == 0) {
+                index = Math.floor(Math.random() * (move.length));
+                index == move.length ? check = 0 : check = 1;
+            }
+            arrVinTemp.push(move[index]);
+            move.splice(index, 1);
+        }
+        return arrVinTemp;
+    }
+    let arrayNominate = getArray(stepArray);
+    //console.log(arrayNominate);
+
+    function getPlayerMoveArray(arrMove) {
+        let playerMoveArray = [...arrMove];
+        const computerMove = [7, 5, 3, 1];
+        computerMove.forEach(move => playerMoveArray.splice(move, 1));
+        return playerMoveArray;
+    }
+
+    let arrayNominatePlayerMove = getPlayerMoveArray(arrayNominate);
+    //console.log(arrayNominatePlayerMove);
+
+    function getArrayCheck(vinPos, testArray) {
+        let testArrayTrue = true;
+        for (let i = 0; i < vinPos.length; i++) {
+            let testArrayTemp = testArray;
+            vinPos[i].forEach(val => testArrayTemp = testArrayTemp.filter(el => el !== val));
+            (testArray.length - testArrayTemp.length) == 3 ? testArrayTrue = false : '';
+        }
+        return testArrayTrue;
+    }
+    let playerMoveNotLose = getArrayCheck(vinPos, arrayNominatePlayerMove);
+    //console.log(playerMoveNotLose);
+    return [playerMoveNotLose, arrayNominate, arrayNominatePlayerMove];
+
+}
+
+function getNotluseArray(stepArray, vinPos) {
+
+    let arrayNominate;
+
+    do { arrayNominate = getArrayTemp(stepArray, vinPos) } while (!arrayNominate[0]);
+    //console.log(arrayNominate);
+    return arrayNominate[1];
 }
 
 
@@ -173,7 +230,8 @@ function allClear() {
         fourthButton,
         fifthButton,
         sixthButton,
-        seventhButton
+        seventhButton,
+        eighthButton
     ];
     setButtonArr.forEach(val => {
         val.innerHTML = '`';
@@ -182,9 +240,11 @@ function allClear() {
         val.classList.remove('btn-light');
         val.classList.add('btn-light')
     });
-    eighthButton.innerHTML = 'X';
+
     pushButtonArr = [];
-    //getGameMode();
+
+    getGameMode();
+
     if (gameMode == 0) {
         pushButtonArr = [8];
     }
@@ -192,51 +252,60 @@ function allClear() {
         pushButtonArr = [];
 
     }
+
+    if (gameMode == 0) {
+        eighthButton.innerHTML = 'X';
+        eighthButton.classList.remove('btn-light');
+        eighthButton.classList.add('btn-info');
+    }
+    else {
+        eighthButton.innerHTML = '`';
+    }
 }
 
 function ticTacToe(num) {
 
+    const setButton = {
+        0: zeroButton,
+        1: firstButton,
+        2: secondButton,
+        3: thirdButton,
+        4: fourthButton,
+        5: fifthButton,
+        6: sixthButton,
+        7: seventhButton,
+        8: eighthButton
+    };
+
+    function buttonChangeClassPlayer(number) {
+        setButton[buttonArr[buttonArrPosition(number)]].classList.remove('btn-light');
+        setButton[buttonArr[buttonArrPosition(number)]].classList.add('btn-warning');
+    }
+
+    function buttonChangeClassComp(number) {
+        setButton[buttonArr[buttonArrPosition(number)]].classList.remove('btn-light');
+        setButton[buttonArr[buttonArrPosition(number)]].classList.add('btn-info');
+    }
+
+    function buttonArrPosition(val) {
+        for (let i = 0; i < buttonArr.length; i++) {
+            if (buttonArr[i] == val) return i;
+        }
+    }
+
     if (gameMode == 0) {
-
-
 
         pushButtonArr.push(num);
         countButtonClick = 1;
 
         if (countGame == 0) { firstMove = num }
 
-        function buttonArrPosition(val) {
-            for (let i = 0; i < buttonArr.length; i++) {
-                if (buttonArr[i] == val) return i;
-            }
-        }
-
         if (countGame == 0) {
             buttonArr = [0, 1, 2, 3, 4, 5, 6, 7];
         }
 
-        const setButton = {
-            0: zeroButton,
-            1: firstButton,
-            2: secondButton,
-            3: thirdButton,
-            4: fourthButton,
-            5: fifthButton,
-            6: sixthButton,
-            7: seventhButton
-        };
-
-        function buttonChangeClassPlayer(number) {
-            setButton[buttonArr[buttonArrPosition(number)]].classList.remove('btn-light');
-            setButton[buttonArr[buttonArrPosition(number)]].classList.add('btn-warning');
-        }
-
-        function buttonChangeClassComp(number) {
-            setButton[buttonArr[buttonArrPosition(number)]].classList.remove('btn-light');
-            setButton[buttonArr[buttonArrPosition(number)]].classList.add('btn-info');
-        }
-
         if (firstMove % 2 == 0) {
+
             if (countGame == 0) {
                 let shiftButton = num;
                 while (shiftButton > 0) {
@@ -244,11 +313,13 @@ function ticTacToe(num) {
                     shiftButton--;
                 }
             };
+
             if (!gameOver) {
                 setButton[buttonArr[buttonArrPosition(num)]].innerHTML = 'O';
                 buttonChangeClassPlayer(num);
                 setTimeout(() => countButtonClick = 0, 100) //1500ms-------------------------------------------
             };
+
             setTimeout(() => {
 
                 if (!gameOver) switch (countGame) {
@@ -311,6 +382,7 @@ function ticTacToe(num) {
 
         }
         else {
+
             if (countGame == 0) {
                 let shiftButton = num;
                 while (shiftButton > 1) {
@@ -369,6 +441,102 @@ function ticTacToe(num) {
         }
     }
     else {
+
+        if (gameMode == 1) {
+
+            pushButtonArr.push(num);
+            countButtonClick = 1;
+
+            if (countGame == 0) { firstMove = num }
+
+            if (countGame == 0) {
+                buttonArr = [0, 1, 2, 3, 4, 5, 6, 7];
+            }
+
+            if (firstMove == 8) {
+
+                if (countGame == 0) { buttonArr.push(num) }
+
+                if (!gameOver) {
+                    setButton[buttonArr[buttonArrPosition(num)]].innerHTML = 'O';
+                    buttonChangeClassPlayer(num);
+                    setTimeout(() => countButtonClick = 0, 100) //1500ms-------------------------------------------
+                };
+
+                setTimeout(() => {
+
+                    if (!gameOver) switch (countGame) {
+                        case (0):
+                            countGame++;
+                            setButton[buttonArr[0]].innerHTML = 'X';
+                            buttonChangeClassComp(buttonArr[0]);
+                            pushButtonArr.push(buttonArr[0]);
+                            break;
+                        case (1):
+                            countGame++;
+                            numberMove = getNotluseArray(pushButtonArr,vinPosZero)[3];
+                            setButton[buttonArr[numberMove]].innerHTML = 'X'
+                            buttonChangeClassComp(buttonArr[numberMove]);
+                            pushButtonArr.push(buttonArr[numberMove]);
+                            break;
+                        case (2):
+                            countGame++;
+                            numberMove = getNotluseArray(pushButtonArr,vinPosZero)[5];
+                            setButton[buttonArr[numberMove]].innerHTML = 'X'
+                            buttonChangeClassComp(buttonArr[numberMove]);
+                            pushButtonArr.push(buttonArr[numberMove]);
+                            break;
+                        case (3):
+                            countGame++;
+                            numberMove = getNotluseArray(pushButtonArr,vinPosZero)[7];
+                            setButton[buttonArr[numberMove]].innerHTML = 'X'
+                            buttonChangeClassComp(buttonArr[numberMove]);
+                            pushButtonArr.push(buttonArr[numberMove]);
+                            gameDraw = true;
+                            break;
+                    }
+                    if (gameOver) eighthButton.innerHTML = 'Lost';
+                    if (gameDraw) {
+                        gameOver = true;
+                        eighthButton.innerHTML = 'Draw'
+                    };
+                }, 80)
+
+            }
+            else {
+                if (firstMove % 2 == 0) {
+                    if (countGame == 0) {
+                        let shiftButton = num;
+                        while (shiftButton > 0) {
+                            buttonArr.push(buttonArr.shift());
+                            shiftButton--;
+                        }
+                    };
+                    if (!gameOver) {
+                        setButton[buttonArr[buttonArrPosition(num)]].innerHTML = 'O';
+                        buttonChangeClassPlayer(num);
+                        setTimeout(() => countButtonClick = 0, 100) //1500ms-------------------------------------------
+                    };
+
+                }
+                else {
+                    if (countGame == 0) {
+                        let shiftButton = num;
+                        while (shiftButton > 1) {
+                            buttonArr.push(buttonArr.shift());
+                            shiftButton--;
+                        }
+                    };
+
+                    if (!gameOver) {
+                        setButton[buttonArr[buttonArrPosition(num)]].innerHTML = 'O';
+                        buttonChangeClassPlayer(num);
+                        setTimeout(() => countButtonClick = 0, 100) //1500ms-----------------------------------------------
+                    };
+                }
+            }
+
+        }
 
     }
 
